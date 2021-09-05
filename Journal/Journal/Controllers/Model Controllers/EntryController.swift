2971aa3f038bfa6ded06f41f96fc8958a0107a72
JoshuaStanfield <joshua.c.stanfield@gmail.com>
2021-09-05 09:34:24 -0600

    Added Equatable to Entry model, CRUD functions, entries SOT, and the shared instance.

--- Journal/Journal/Controllers/Model Controllers/EntryController.swift
+++ Journal/Journal/Controllers/Model Controllers/EntryController.swift
@@ -9,4 +9,22 @@ import Foundation
 
 class EntryController {
     
+    // MARK: - Shared Instance
+    let shared = EntryController()
+    
+    // MARK: - SOT
+    var entries: [Entry] = []
+    
+    // MARK: - CRUD Functions
+    ///Create an entry and add it to the entries array.
+    func createEntryWith(title: String, body: String) {
+        let entry = Entry(title: title, body: body)
+        entries.append(entry)
+    }
+    
+    ///Find the index of the entry and remove it at that index.
+    func delete(entry: Entry) {
+        guard let index = entries.firstIndex(of: entry) else {return}
+        entries.remove(at: index)
+    }
 }
